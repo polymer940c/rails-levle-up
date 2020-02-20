da04c17427ba82eb8a3b120418c95fef510dff85
Kevin Hu <hu.kevin.w@gmail.com>
2020-02-20 16:54:17 -0500

    Post new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:show, :index]
+  resources :posts, only: [:show, :index, :new]
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
